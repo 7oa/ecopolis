8f0e57feddc3be19817dbc2307041407f751198c
7oa <semenova.7oa@yandex.ru>
2017-08-02 19:06:54 +0300

    new

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -1,26 +1,52 @@
 // js jquery
 $(document).ready(function() {
-	//popup
-	$('.js-open-callback').click( function(event){
-		event.preventDefault();
-		$('#callback__overlay').fadeIn(400,
-			function(){
-				$('#callback')
-					.css('display', 'block')
-					.animate({opacity: 1}, 200);
-				$('.callback-inner').animate({"margin-top": "30px"}, 200);
-			});
-		$("body").css('overflow-y','hidden');
+	//кнопка наверх
+	var top_show = 350; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+	var delay = 1000; // Задержка прокрутки
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > top_show) $('#top').fadeIn();
+		else $('#top').fadeOut();
 	});
-	$('#callback__close, #callback__overlay').click( function(){
-		$('#callback')
-			.animate({opacity: 0}, 200,
-				function(){
-					$(this).css('display', 'none');
-					$('#callback__overlay').fadeOut(400);
-				}
-			);
-		$('.callback-inner').animate({"margin-top": "0px"}, 200);
-		$("body").css('overflow-y','auto');
+	$('#top').click(function () {
+		$('body, html').animate({
+			scrollTop: 0
+		}, delay);
 	});
+	//end кнопка наверх
+
+
+	//слайдер на главной
+	var swiper = new Swiper('.index-swiper', {
+		pagination: '.index-swiper-pagination',
+		paginationClickable: true,
+		speed: 1000,
+		autoplay: 5500,
+		autoplayDisableOnInteraction: false,
+		loop: true
+	});
+	//end слайдер на главной
+
+	//плавное появление
+	if($('.js-show_up').length>0){
+		$('.js-show_up').addClass("hidden").viewportChecker({
+			classToAdd: 'visible animated fadeInUp',
+			classToRemove: 'hidden',
+			offset: 100
+		});
+	}
+	if($('.js-show_left').length>0){
+		$('.js-show_left').addClass("hidden").viewportChecker({
+			classToAdd: 'visible animated fadeInLeft',
+			classToRemove: 'hidden',
+			offset: 100
+		});
+	}
+	if($('.js-show_right').length>0){
+		$('.js-show_right').addClass("hidden").viewportChecker({
+			classToAdd: 'visible animated fadeInRight',
+			classToRemove: 'hidden',
+			offset: 100
+		});
+	}
+	//end плавное появление
 });
